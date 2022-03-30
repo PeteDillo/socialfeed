f0089e92e398b2003fa6da20289998f7b63824da
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2022-03-29 21:21:40 -0500

    added the post feature. via CreatePost component.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { useState } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import DisplayPost from "./components/DisplayPost/DisplayPost";
+import CreatePost from "./components/CreatePost/CreatePost";
 
 function App() {
   const [posts, setPost] = useState([
     {
       body: "I love life or something",
       user: "Pedro D",
-      date: "03-29-2022",
+      date: "2022-03-30",
     },
   ]);
+
+
+  function addNewPost(post) {
+    let temp = [...posts, post];
+    setPost(temp);
+  }
+
+
 
   return (
     <div className="App">
       <NavBar />
-      
+      <CreatePost  setPost={addNewPost}/>
       <DisplayPost posts={posts} />
     </div>
   );
--- src/components/CreatePost/CreatePost.jsx
+++ src/components/CreatePost/CreatePost.jsx
@@ -1,11 +1,60 @@
+import React, { useState, useEffect } from 'react';
+
 const CreatePost = (props) => {
-    return ( 
-        <form>
-            <div className="form-group">
-                
-            </div>
-        </form>
-     );
-}
- 
+
+    const [user, setUser] = useState('');
+    const [date, setDate] = useState("");
+    const [body, setBody] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let newPost = {
+        body: body,
+      date: date,
+      user: user
+    };
+    console.log({ newPost });
+    props.setPost(newPost);
+  }
+
+  return (
+    <form onSubmit={handleSubmit} >
+      <div>
+        <label>Whatcha wanna sayy ay?</label>
+        <input
+          className="form-control"
+          type="text"
+          value={body}
+          onChange={(event) => setBody(event.target.value)}
+        ></input>
+      </div>
+      <div>
+        <label>Who wanna say it?</label>
+        <input
+          className="form-control"
+          type="text"
+          value={user}
+          onChange={(event) => setUser(event.target.value)}
+        ></input>
+      </div>
+      <div className="form-group">
+        <label>Date</label>
+        <input
+          className="form-control"
+          type="date"
+          value={date}
+          onChange={(event) => setDate(event.target.value)}
+        ></input>
+      </div>
+      <button
+        type="submit"
+        className="btn btn-primary"
+        style={{ "margin-top": "1em" }}
+      >
+        Add
+      </button>
+    </form>
+  );
+};
+
 export default CreatePost;
